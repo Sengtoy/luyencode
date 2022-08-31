f66e2e682758aab66a524442422a34eac47294a0
Sengtoy <111959218+Sengtoy@users.noreply.github.com>
2022-08-31 14:54:00 +0700

    Update VL03.cs

--- VL03.cs
+++ VL03.cs
@@ -1,17 +1,18 @@
 using System;
+
 namespace VL03
 {
     class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
-           var s = Console.ReadLine();
-            int n = Convert.ToInt32(s);
-            int sum = 0;
-            for(int i = 2; i <= n; i++)
-                sum = sum + i;
-            Console.WriteLine("{0}", sum);
-            Console.ReadLine();  
+            string s1 = Console.ReadLine();
+            int n = Convert.ToInt32(s1);
+            int s = 0;
+            for(int i=2; i<=n; i++)
+                s = s + i;
+                s = s + 2 * n;
+
+            Console.WriteLine(s);
         }
     }
-}
