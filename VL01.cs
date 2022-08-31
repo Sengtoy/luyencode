230c349b4664f7c4ef1a9a76d11507a2c0dbcbf9
Sengtoy <111959218+Sengtoy@users.noreply.github.com>
2022-08-31 14:54:46 +0700

    Update VL01.cs

--- VL01.cs
+++ VL01.cs
@@ -1,15 +1,14 @@
 using System;
 namespace VL01{
-class Program {
-  static void Main(string[] args) {
-      var s = Console.ReadLine();
-      var s1 = s.Split(' ');
-      int a = Convert.ToInt32(s1[0]);
-      int b = Convert.ToInt32(s1[1]);
-      for (int i = a; i <= b; i++){
-    Console.Write("{0}",i);
-      }
-      Console.ReadLine();
-  }
- }
+public class Program
+{
+    static void Main(string[] args)
+    {
+       string s1 = Console.ReadLine();
+            var s2 = s1.Split(' ');
+            int a = Convert.ToInt32(s2[0]);
+            int b = Convert.ToInt32(s2[1]);
+            for (int i = a; i <= b; i++)    
+            Console.Write("{0} ", i);
+    }
 }
